baa807d9373c87fd59b4e615a0a8ae506f910806
RaisaKhaliulina <92810216+RaisaKhaliulina@users.noreply.github.com>
2021-10-28 00:42:09 +0300

    Add files via upload

---
+++ counter.jsx
@@ -0,0 +1,49 @@
+import React,{useState} from 'react';
+
+const Counter = () => {
+
+    const[count, setCount]=useState(0)
+    const tags = ["tag1","tag2","tag3"]
+
+    const formCount = () => {
+        return count ===0 ? "Ноль" : count
+    }
+
+    const getBageclasses = () => {
+    let classes = "badge m-2 bg-"
+    classes +=count === 0 ? "primary":"danger"
+    return classes
+    }
+    const renderTags= ()=> {
+        if(tags.length===0)return "Тегов не найдено"
+        return tags.map((tag) =>
+        <li key={tag} >{tag}</li>)
+        }
+    const handleIncrement = () => {
+        setCount(count+1)
+    }
+    const handleDecrement = () => {
+        count < 1 ? setCount(count):
+        setCount(count-1)
+    }
+    return ( 
+      <>
+        {tags.length ===0 && "Теги не найдены"}
+        {renderTags()}
+        
+        <span className={getBageclasses()}>{formCount()}</span> 
+        <button 
+          onClick={handleIncrement}
+          className="btn btn-secondary btn-sm m-2">
+          Increment
+        </button>
+        <button
+          onClick={handleDecrement}
+          className="btn btn-secondary  btn-sm">
+          Decrement    
+        </button>
+      </>
+    );
+};
+ 
+export default Counter;
